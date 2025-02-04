9b78b64d65ef54e7bd4548279e060afb1d0692da
MatteoSbarluzzi <matteosbarluzzi1@gmail.com>
2025-02-04 15:37:40 +0100

    Selfwork PHP 3 corretto

--- Selfwork3.php
+++ Selfwork3.php
@@ -1,43 +1,54 @@
 <?php
 
-
 $words1 = [
-  'vostro',
-  67,
-  'essere',
-  'colle',
-  'mi',
-  'sempre',
-  [
-    'oscura',
-    'era',
-    89,
+    'vostro',
+    67,
+    'essere',
+    'colle',
+    'mi',
+    'sempre',
     [
-      'mezzo',
-      'E'
+        'oscura',
+        'era',
+        89,
+        [
+            'mezzo',
+            'E'
+        ],
+        'ritrovai',
+        'per'
     ],
-    'ritrovai',
-    'per'
-  ],
-  'diritta'
+    'diritta'
 ];
 
 $words2 = [
-  'elemento1' => 25.89,
-  'elemento2' => 'possa',
-  'elemento3' => [
-    'Virgilio',
-    'Favore',
-    'favore',
-    ['fortuna']
-  ],
-  'fine' => '!'
+    'elemento1' => 25.89,
+    'elemento2' => 'possa',
+    'elemento3' => [
+        'Virgilio',
+        'Favore',
+        'favore',
+        ['fortuna']
+    ],
+    'fine' => '!'
+];
+
+// Parole mancanti
+$words3 = [
+    ['Nel'],
+    ['di camin'],
+    ['di nostra vita'],
+    ['per una selva'],
+    [", che' la"], 
+    ['via'],
+    ['smarrita'],
 ];
 
 
-$frase = "Nel " . $words1[7][3][0] . " di cammin di nostra vita " . $words1[4] . " " . $words1[8] . " per una selva " . $words1[7][0] . ", che' la " . $words1[10] . " via " . $words1[7][1] . " smarrita";
+$results = $words3[0][0] . " " . $words1[7][3][0] . " " . $words3[1][0] . " " . $words3[2][0] . " " . $words1[4] . " " . $words1[8] . " " . $words1[7][4] . " " . $words1[7][5] . " " . $words3[3][0] . " " . $words1[7][0] . " " . $words3[4][0] . " " . $words1[10] . " " . $words3[5][0] . " " . $words1[7][12] . " " . $words3[6][0];
 
 
-echo $frase;
+echo $results;
 
 ?>
+
